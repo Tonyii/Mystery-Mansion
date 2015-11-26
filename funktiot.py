bf3af9f47e0f8f536dcc9bc106ef8c2e78d89df1
Eetu Kaivola <eetu.kaivola@gmail.com>
2015-11-26 09:57:10 +0200

    referenssipeli päivittyy

--- funktiot.py
+++ funktiot.py
@@ -2,3 +2,199 @@
 
 # -*- coding: utf-8 -*-
 
+import mysql.connector
+
+#
+# Avaa tietokantayhteyden.
+#
+def avaa_tietokanta(hostname, uname, pswd):
+    return mysql.connector.connect(
+        host=hostname, user=uname, passwd=pswd, db="peli",buffered=True
+    )
+
+#
+# Palauttaa pelaajan nykyisen sijainnin.
+#
+def sijainti(db):
+    cur = db.cursor()
+    cur.execute("select sijainti from pelihahmo where id = 1");
+    return (cur.fetchone())[0]
+
+#
+# Palauttaa annetun paikan kuvauksen.
+#
+def kuvaus(db, id):
+    cur = db.cursor()
+    cur.execute("select kuvaus from paikka where id = " + str(id))
+    return (cur.fetchone())[0]
+
+#
+# Palauttaa listan mahdollisista kulkusuunnista.
+#
+def suunnat(db, id):
+    cur = db.cursor()
+    cur.execute("select p, e, i, l from paikka where id = " + str(id))
+    rivi = cur.fetchone()
+    kompassi = ['p', 'e', 'i', 'l']
+    suunnat = []
+    for i in range(0, 4):
+        if rivi[i] is not None:
+            suunnat.append(kompassi[i])
+    return suunnat
+
+#
+# Palauttaa listan annetun paikan esineiden nimista.
+#
+def esineet(db, id):
+    cur = db.cursor()
+    cur.execute("select kuvaus from objekti where sijainti = " + str(id))
+    esineet = []
+    tulos = cur.fetchall()
+    for rivi in tulos:
+        esineet.append(rivi[0])
+    return esineet
+
+#
+# Palauttaa tiedon siita, onko roistoon tormatty aikaisemmin.
+#
+def onko_roisto_tavattu(db):
+    cur = db.cursor()
+    cur.execute("select arvo from juonenkaanne where id = 1")
+    return (cur.fetchone())[0]
+
+#
+# Paivittaa tiedon siita, etta roisto on poistunut.
+#
+def poista_roisto(db):
+    cur = db.cursor()
+    cur.execute("update juonenkaanne set arvo = 1 where id = 1")
+
+#
+# Tallettaa tiedon siita, etta roisto on hoideltu.
+#
+def roisto_on_hoideltu(db):
+    cur = db.cursor()
+    cur.execute("update juonenkaanne set arvo = 1 where id = 1")
+
+#
+# Palauttaa tiedon siita kuinka monta kertaa kayty metsaaukealla.
+#
+def kerrat_aukealla(db):
+    cur = db.cursor()
+    cur.execute("select arvo from juonenkaanne where id = 2")
+    return (cur.fetchone())[0]
+
+#
+# Tallettaa kaynnin metsaaukealla.
+#
+def talleta_kaynti_aukealla(db):
+    cur = db.cursor()
+    cur.execute("update juonenkaanne set arvo = arvo + 1 where id = 2")
+
+#
+# Palauttaa annetun objektin sijainnin (None, jos hallussa)
+#
+def objektin_sijainti(db, kuvaus):
+    cur = db.cursor()
+    cur.execute(
+        "select sijainti from objekti where kuvaus = '" + kuvaus + "'"
+    )
+    if cur.rowcount == 0:
+        return None
+    return (cur.fetchone())[0]
+
+#
+# Avaa yhteyden annetusta huoneesta annettuun suuntaan (p/e/i/l)
+#
+def avaa_suunta(db, lahto, tulo, suunta):
+    cur = db.cursor()
+    cur.execute(
+        "update paikka set " +
+        suunta + " = " + str(tulo) + " " +
+        "where id = " + str(lahto)
+    )
+
+#
+# Palauttaa pelaajan hallussa olevien tavaroiden painon.
+#
+def laske_paino(db):
+    cur = db.cursor()
+    cur.execute("select paino from objekti where omistaja = 1")
+    tulos = cur.fetchall()
+    paino = 0
+    for rivi in tulos:
+        paino += float(rivi[0])
+    return paino
+
+#
+# Palauttaa annetun objektin painon.
+#
+def objektin_paino(db, kuvaus):
+    cur = db.cursor()
+    cur.execute(
+        "select paino from objekti where kuvaus = '" + kuvaus + "'"
+    )
+    return float((cur.fetchone())[0])
+
+#
+# Komento: siirtyminen (p, e, i, l)
+#
+# Palauttaa 1 jos siirtyminen onnistui, 0 muuten.
+#
+def komento_siirry(db, sijainti, suunta):
+    cur1 = db.cursor()
+    cur1.execute(
+        "select " + suunta + " from paikka where id = " + str(sijainti)
+    )
+    rivi = cur1.fetchone()
+    if rivi[0] is None:
+        return 0
+    uusi_sijainti = str(rivi[0])
+    cur2 = db.cursor()
+    cur2.execute(
+        "update pelihahmo set sijainti = " + uusi_sijainti + " where id = 1")
+    return 1
+
+#
+# Komento: ota
+#
+def komento_ota(db, kuvaus):
+    cur1 = db.cursor()
+    cur1.execute(
+        "update objekti set omistaja = 1 where kuvaus = '" + kuvaus + "'"
+    )
+    cur2 = db.cursor()
+    cur2.execute(
+        "update objekti set sijainti = NULL where kuvaus = '" + kuvaus + "'"
+    )
+
+#
+# Komento: kamat
+#
+def komento_kamat(db):
+    cur = db.cursor()
+    cur.execute("select kuvaus from objekti where omistaja = 1")
+    kamat = []
+    tulos = cur.fetchall()
+    for rivi in tulos:
+        kamat.append(rivi[0])
+    return kamat
+
+#
+# Komento: tiputa
+#
+def komento_tiputa(db, kuvaus, sijainti):
+    cur1 = db.cursor()
+    cur1.execute(
+        "update objekti set sijainti = " +
+        str(sijainti) +
+        " where kuvaus = '" + kuvaus + "' and omistaja = 1"
+    )
+    cur2 = db.cursor()
+    cur2.execute(
+        "update objekti set omistaja = NULL where kuvaus = '" +
+        kuvaus + "' and omistaja = 1"
+    )
+    return cur2.rowcount
+
+
--- peli.py
+++ peli.py
@@ -2,3 +2,151 @@
 
 # -*- coding: utf-8 -*-
 
+###############################################################################
+#
+#  S E I K K A I L U P E L I
+#
+# (c) Miikka Maki-Uuro, 2014
+#
+###############################################################################
+import getpass
+import mysql.connector
+import funktiot
+
+#
+# Luodaan tietokantayhteys.
+#
+host = input("palvelin : ")
+ktunnus = input("ktunnus  : ")
+salasana = getpass.getpass("salasana : ")
+db = funktiot.avaa_tietokanta(host, ktunnus, salasana)
+
+#
+# Pelin paasilmukka:
+#
+# - tulostetaan paikan kuvaus
+# - tulostetaan mahdolliset kulkusuunnat
+# - tulostetaan paikalla olevat esineet
+# - tulostetaan mahdolliset erikoistapahtumat
+# - luetaan ja jasennetaan kayttajan komento
+# - suoritetaan komento
+#
+lopetus = 0
+while lopetus == 0:
+
+    # Hae sijainti ja tulosta kuvaus.
+    sijainti = funktiot.sijainti(db)
+    kuvaus = funktiot.kuvaus(db, sijainti)
+    print("")
+    print(kuvaus)
+
+    # Tulosta mahdolliset kulkusuunnat.
+    suunnat = funktiot.suunnat(db, sijainti)
+    if len(suunnat) > 0:
+        print("Voit kulkea:", suunnat)
+
+    # Tulosta paikalla olevat esineet.
+    esineet = funktiot.esineet(db, sijainti)
+    if len(esineet) > 0:
+        print("Taalla on:", esineet)
+
+    # Erikoistapahtuma 1: roisto metsatiella.
+    if sijainti == 6 and funktiot.onko_roisto_tavattu(db) == 0:
+        print("Taalla on resuinen roisto! Han ei paasta sinua etenemaan.")
+
+    # Erikoistapahtuma 2: peli paattyy 3. kerralla metsaaukealla.
+    if sijainti == 7:
+        funktiot.talleta_kaynti_aukealla(db)
+        if funktiot.kerrat_aukealla(db) == 3:
+            print("Ihana tunne valtaa sinut... elama hymyilee!")
+            print("Peli on paattynyt. Olet sankari!!!!!!!!!!!!")
+            lopetus = 1
+            continue
+
+    # Luetaan ja jasennellaan kayttajan komento.
+    print("")
+    komentorivi = input("> ")
+    if komentorivi == "":
+        continue
+    komentolista = komentorivi.split()
+    if len(komentolista) == 1:
+        verbi = komentolista[0]
+        objekti = ""
+    elif len(komentolista) == 2:
+        verbi = komentolista[0]
+        objekti = komentolista[1]
+    else:
+        print("Ymmarran vain yhden tai kahden sanan komentoja.")
+
+    # Komento: p/e/i/l
+    if verbi == "p" or verbi == "e" or verbi == "i" or verbi == "l":
+        if funktiot.komento_siirry(db, sijainti, verbi) == 0:
+            print("Sinne suuntaan ei paase!")
+
+    # Komento: ota
+    elif verbi == "ota":
+        if objekti == "":
+            print("Mita olisi pitanyt ottaa?!")
+        else:
+            objektin_sijainti = funktiot.objektin_sijainti(db, objekti)
+            if objektin_sijainti is None or objektin_sijainti != sijainti:
+                print(objekti, "ei ole otettavissa!")
+            elif funktiot.laske_paino(db) + funktiot.objektin_paino(db, objekti) > 10:
+                print("Liian painava!")
+            else:
+                funktiot.komento_ota(db, objekti)
+                print(objekti, "otettu.")
+
+    # Komento: kamat
+    elif verbi == "kamat":
+        kamat = funktiot.komento_kamat(db)
+        print("Sinulla on:", kamat)
+
+    # Komento: tiputa
+    elif verbi == "tiputa":
+        if objekti == "":
+            print("Mita olisi pitanyt tiputtaa?!")
+        elif funktiot.komento_tiputa(db, objekti, sijainti) == 0:
+            print(objekti, "ei tiputettavissa!")
+        else:
+            print(objekti, "tiputettu.")
+
+    # Komento: avaa
+    elif verbi == "avaa":
+        if objekti != "ovi":
+            print("Mita olisi pitanyt avata?!")
+        elif sijainti != 1 and sijainti != 2:
+            print("Ei taalla ole ovea.")
+        elif funktiot.objektin_sijainti(db, "avain") != None:
+            print("Ei sinulla ole avainta.")
+        else:
+            funktiot.avaa_suunta(db, 1, 2, "i")
+            funktiot.avaa_suunta(db, 2, 1, "l")
+            print("Ovi on auki.")
+
+    # Komento: anna
+    elif verbi == "anna":
+        if objekti == "":
+            print("Mita olisi pitanyt antaa?!")
+        elif funktiot.objektin_sijainti(db, objekti) is not None:
+            print("Ei sinulla ole sita.")
+        elif objekti != "kolikko" or sijainti != 6 or funktiot.onko_roisto_tavattu(db) != 0:
+            print("Ei vaikutusta.")
+        else:
+            funktiot.poista_roisto(db)
+            funktiot.avaa_suunta(db, 6, 7, "i")
+            funktiot.avaa_suunta(db, 7, 6, "l")
+            print("Roisto ottaa tyytyvaisena kolikon ja haipyy metsaan!")
+
+    # Komento: lopeta
+    elif verbi == "lopeta":
+        lopetus = 1
+
+    # Tuntematon komento.
+    else:
+        print("En ymmarra...")
+
+    # Jos haluat tallettaa tilanteen joka kierroksella: db.commit()
+
+print("Kiitos pelista. Nahdaan taas!")
+# Jos haluat etta tilanne talletetaan pelin paatteeksi: db.commit()
