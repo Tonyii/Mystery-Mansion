7d04aa4744e10524fd4c4dfce042d741258727ee
Tonyii <tony.kuitunen@gmail.com>
2015-11-30 13:37:14 +0200

    tietokantaan lisätty liikkumiskomennot

--- tietokanta.sql
+++ tietokanta.sql
@@ -36,6 +36,11 @@ create table NPC(
 	primary key (NPCID),
 	foreign key (Location) references Room(RoomID));
 
+create table Synocmd(
+	Nimi varchar(20) not null,
+	Synonyymi varchar(20) not null,
+	primary key (Synonyymi));
+
 insert into Room values (1, "guestroom"), (2, "garage"), (3, "corridor"),
 			(4, "maidroom"), (5, "office"), (6, "kitchen"), (7, "stairs"), (8, "ballroom"),
             (9, "bathroom"), (10, "master bedroom"), (11, "study"), (12, "attic");
@@ -44,3 +49,4 @@ insert into Player values (1, 1);
 insert into NPC values (1, "Groundskeeper Willy", 2, 1), (2, "Maid Penelope", 4, 1), (3, "Chef Gordon", 6, 1), (4, "Jeeves the Butler", 5, 1), (5, "Lady Sonya", 10, 1);
 insert into Item values (1, "whiskey", null, 6), (2, "torn page", null, 1), (3, "safe", null, 11), (4, "combination", null, 10), (5, "dark spellbook", null, 11)
 
+insert into Synocmd values ('go', 'go'), ('go','move'), ('go','exit'),( 'go','walk'),( 'go','travel'),( 'go', 'climb'),( 'go', 'crawl'),( 'go', 'run');
