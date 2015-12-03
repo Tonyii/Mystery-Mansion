b068ebd4cf9cc06d87554655fd0e8e5b975125e6
dalud <jaakko.niska@gmail.com>
2015-12-03 13:27:34 +0200

    intro tehty

--- textadventure/oma_funktiot.py
+++ textadventure/oma_funktiot.py
@@ -112,7 +112,7 @@ def conversation(db, suspect):
     elif trust == 2 :
         if personid == 1:
             answer = str("\n\"I heard her, miss Penelope. In the attic. She did some weird things! *hiccup* I went to war\n"
-                     "and I never heard anything as scary as that.  \"")
+                     "and I never heard anything as scary as that.\"")
             return answer
 
 
