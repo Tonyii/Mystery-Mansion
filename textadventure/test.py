c52875c828dba9acd2d28c00c19aafcc9fa255ee
Eetu Kaivola <eetu.kaivola@gmail.com>
2015-12-04 12:50:04 +0200

    korjattu talk to eri huoneissa.

--- textadventure/test.py
+++ textadventure/test.py
@@ -7,14 +7,8 @@ pswd = 'mm'
 db = oma_funktiot.open_database(hostname, uname, pswd)
 
 cursor=db.cursor()
-cursor.execute("select * from plot")
+send = "select npc.npcid, npc.trust from SynoPerson, npc where synoperson.Synonyymi = '" + suspect + "' and where npc.location in (select location from player) and Synoperson.personID = npc.npcid"
+cursor.execute(send)
 plot=cursor.fetchone()
 print(plot)
 
-while plot == (0, 0, 0, 0, 0):
-    step=input("what now then?")
-    if step == 'go':
-        cursor.execute("update plot set state1=1")
-        cursor.execute("select * from plot")
-        plot=cursor.fetchone()
-        print (plot)
